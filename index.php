4539014ea350e2eb5d02de6f26d05e9937c08213
Vesihiisi <alicia@fagerving.se>
2015-10-03 20:01:15 +0200

    rm stuff that didn't work

--- index.php
+++ index.php
@@ -77,14 +77,13 @@ if ($_POST)
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css">
       <link href="css/jquery.tagit.css" rel="stylesheet" type="text/css">
-      <link href="css/jquery-confirm.css" rel="stylesheet" type="text/css">
+
       <link rel="stylesheet" type="text/css" href="css/style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js" type="text/javascript" charset="utf-8"></script>
       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
       <script src="js/jquery.sticky.js"></script>
       <script src="js/tag-it.min.js"></script>
-      <script src="js/jquery-confirm.min.js"></script>
       <script src="js/script.js"></script>
       <title>super bookmarker</title>
   </head>
