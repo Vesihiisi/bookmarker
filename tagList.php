919b7cdef9e4288b27fdcb69cc330a8bec507b5d
Vesihiisi <alicia@fagerving.se>
2015-10-03 11:19:08 +0200

    more list styling

--- tagList.php
+++ tagList.php
@@ -18,15 +18,17 @@ function getTagsOfUser($userID)
 }
 
 function printTagList($queryResult) {
+    echo "<div class='tag-list'>";
+    echo "<ul>";
     foreach ($queryResult as $row) {
         $tag = $row["tag"];
         $count = $row["count"];
-        echo "<div class='tag-list'>";
-        echo "<ul>";
         echo "<li>$tag <span class='tag-count'>$count</span></li>";
-        echo "</ul>";
-        echo "</div>";
+
+
     }
+    echo "</ul>";
+    echo "</div>";
 }
 
 $tags = getTagsOfUser($userID);
