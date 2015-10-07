e0d477f768914063cc5a79d9b8f2d4abe2f6657d
Vesihiisi <alicia@fagerving.se>
2015-10-07 16:22:29 +0200

    i don't really remember what happened here

--- delete.php
+++ delete.php
@@ -1,19 +1,7 @@
 <?php
 include "base.php";
 
-function deleteConnections($id)
-{
-    $query = "DELETE from taggedlinks where linkID = ?";
-    $params = [$id];
-    editQuery($query, $params);
-}
 
-function deleteEntry($id)
-{
-    $query = "DELETE from links where linkID = ?";
-    $params = [$id];
-    editQuery($query, $params);
-}
 
 $id = $_POST['linkID'];
 deleteEntry($id);
