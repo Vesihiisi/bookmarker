e93d27ace4973c5edde1da3991a05102bd293dbf
Vesihiisi <alicia@fagerving.se>
2015-10-12 12:43:18 +0200

    style register form

--- register.php
+++ register.php
@@ -1,13 +1,38 @@
 <?php include "base.php"; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">  
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
- 
-<title></title>
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>  
-<body>  
-<div id="main">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include 'head.php';?>
+</head>
+
+<body>
+<div class="container">
+
+
+
+<div class="row top-row">
+<div class="col-sm-12">
+
+<div class="navbar-header">
+&nbsp;
+</div>
+
+
+
+</div>
+</div>
+
+
+<div class='page-header row'>
+
+
+<div class="col-sm-4">
+<h1>Super bookmarker</h1>
+</div>
+
+
+
+</div>
 <?php
 
 if(!empty($_POST['username']) && !empty($_POST['password'])) {
@@ -39,21 +64,41 @@ if(!empty($_POST['username']) && !empty($_POST['password'])) {
 
     ?>
      
-   <h1>Register</h1>
+
+
+<div class="row">
+
+<div class="col-sm-3">
+</div>
+
+<div class="col-sm-6">
+
+
+
+
+   <h2>Sign up</h2>
      
      
-    <form method="post" action="register.php" name="registerform" id="registerform">
+    <form method="post" action="register.php" name="registerform" id="registerform" class="form-signin">
     <fieldset>
-        <label for="username">Username:</label><input type="text" name="username" id="username" /><br />
-        <label for="password">Password:</label><input type="password" name="password" id="password" /><br />
-        insert repeat password here!!!!!<br>
-        <label for="email">Email Address:</label><input type="text" name="email" id="email" /><br />
-        <input type="submit" name="register" id="register" value="Register" />
+        <label for="username" class="sr-only">Username</label>
+        <input type="text" name="username" id="username" placeholder ="Username" class="form-control">
+        <label for="password" class="sr-only">Password</label>
+        <input type="password" name="password" id="password" placeholder ="Password" class="form-control">
+        <label for="email" class="sr-only">Email address</label>
+        <input type="email" name="email" id="email" placeholder ="E-mail" class="form-control">
+        <div class="lower-row">
+        <button class="btn btn-circle btn-primary pull-right" type="submit" name="register" id="register" value="Register"><span class="glyphicon glyphicon-ok"></span></button>
+        </div>
     </fieldset>
     </form>
      
 
- 
+ </div>
+<div class="col-sm-3">
+</div>
+</div>
+</div>
 </div>
 </body>
 </html>
