7b90c8774f2dd8b1f4351282f12e0c0979d43f6d
Vesihiisi <alicia@fagerving.se>
2015-10-31 10:57:11 +0100

    Use curl to download a page, to avoid 403

--- base.php
+++ base.php
@@ -48,9 +48,21 @@ function editQuery($query, $params)
     return $db->lastInsertId();
 }
 
+function getPage($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
 function getPageTitle($url)
 {
-    $dom = HtmlDomParser::file_get_html($url);
+    $dom = HtmlDomParser::str_get_html(getPage($url));
     return $dom->find('title', 0)->innertext;
 }
 
